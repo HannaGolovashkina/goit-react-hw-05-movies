7b3200ecb67c1c1c7f21ceb4bcc013f39fd7289c
Hanna Golovashkina <108117285+HannaGolovashkina@users.noreply.github.com>
2023-02-09 11:45:26 +0200

    Update css.js

--- src/utilities/variables/css.js
+++ src/utilities/variables/css.js
@@ -1,10 +1,10 @@
-export const mainTextColor = '#161616';
-export const secondTextColor = '#fafafa';
-export const accentTextColor = '#8e0e00';
+export const mainTextColor = '#01b4e4';
+export const secondTextColor = '##0d253f';
+export const accentTextColor = '##90cea1';
 
-export const mainBgColor = '#fafafa';
-export const secondBgColor = '#161616';
-export const accentBgColor = '#8e0e00';
+export const mainBgColor = '#0d253f';
+export const secondBgColor = '##01b4e4';
+export const accentBgColor = '##90cea1';
 
 export const mainHoverEfect = '250ms linear';
-export const mainGradient = 'linear-gradient(to right, #8e0e00, #1f1c18)';
+export const mainGradient = 'linear-gradient(to right, #90cea1, #01b4e4)';
